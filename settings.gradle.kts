1bc75e3f4f24934c13324ce1e92343f907d88df1
abdel504 <s1127763@student.hsleiden.nl>
2025-11-20 16:48:00 +0100

    Eerste commit test 252

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ApplicationFinancial02"
+include(":app")
+include(":applicatie_financial_fail_252")
+include(":applicatie_financial_pass_252")
